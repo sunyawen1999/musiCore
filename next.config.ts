e68619cdcc701bb7d5e1586de109558fc02c9587
yawen <sunyawen1999@gmail.com>
2025-01-12 20:31:43 -0500

    Fetch list

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "nfvvoimpickxikdwyasi.supabase.io",
+      "nfvvoimpickxikdwyasi.supabase.co"
+    ]
+  }
 };
 
 export default nextConfig;
