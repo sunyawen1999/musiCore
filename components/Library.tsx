219df665a146363c3740f61dc9c2ba76c2663656
yawen <sunyawen1999@gmail.com>
2025-01-14 13:35:10 -0500

    List display

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,10 +3,19 @@
 import useAuthModal from "@/hooks/useAuthModal";
 import useUploadModal from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser"
+import { Song } from "@/types";
+
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[]
+}
+
+const Library: React.FC<LibraryProps> = ({
+  songs
+}) => {
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user, subscription } = useUser();
@@ -66,7 +75,13 @@ const Library = () => {
               mt-4
               px-3"
             >
-              List of Songs!
+              {songs.map((item) => (
+                <MediaItem
+                  onClick={() => {}}
+                  key={item.id}
+                  data={item}
+                />
+              ))}
             </div>
         </div>
      );
